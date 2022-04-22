08d132e98c3296ffb4203cecbdd8b2e83c68dea6
Rustin Shamloo <76240354+rustinshamloo@users.noreply.github.com>
2022-04-22 15:09:24 -0700

    Update streamlit-test.py

--- streamlit-test.py
+++ streamlit-test.py
@@ -17,7 +17,7 @@ with tables:
     st.text('Take a look at scams we have aggregated in our database: ')
 
 # take df from scams.py
-    data = pd.read_csv('scams.csv')
+    data = pd.read_csv('scams.csv', index_col=False)
     st.write(data)
 
     # num_scams = pd.DataFrame(data['URL'].value_counts())
@@ -27,12 +27,13 @@ with tables:
 # show df too, make columns visible, maybe agg by coin or something??
 
 with questions:
-    st.subheader('Tell us more: ')
+    st.subheader('Tell us more here: ')
 
 drop = st.selectbox('How many years have you been in crypto?', options=['Select one', '<1', '1-3', '3-5', '6+'])
 input_q1 = st.text_input('Which coins were targeted in this scam?')
 input_q2 = st.text_input('What amount of those coins were targeted in this scam?')
 input_q3 = st.text_input('Which wallet do you use to store your coins?')
+input_q4 = st.text_input('Please provide a description below.')
 
 
 # URLs to crypto scam alrts like whale alert or scam alert, etc?
